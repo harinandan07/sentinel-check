f2daba85979e191c6891646d12d00fb1003e9352
harinandan07 <harinandan.kunwer@gmail.com>
2025-06-13 19:28:02 +0100

    Trigger destroy plan by removing EC2 resource

--- instance.tf
+++ instance.tf
@@ -1,19 +1,20 @@
 
-# Configure the AWS Provider
-provider "aws" {
-  region  = "eu-north-1" # Specify the AWS region you want to work with
- #profile = var.profile != "" ? var.profile : null
-}
+# # Configure the AWS Provider
+# provider "aws" {
+#   region  = "eu-north-1" # Specify the AWS region you want to work with
+#  #profile = var.profile != "" ? var.profile : null
+#  profile = "awstest1947"
+# }
 
 
-resource "aws_instance" "AWS-terraform" {
+# resource "aws_instance" "AWS-terraform" {
  
-  ami = "ami-079ae45378903f993"
+#   ami = "ami-079ae45378903f993"
 
-  instance_type = "t3.micro"
+#   instance_type = "t3.micro"
 
-  # tags = {
-  #   Name = "AWS-Terraform-test"
-  # }
+#   # tags = {
+#   #   Name = "AWS-Terraform-test"
+#   # }
 
-}
+# }
